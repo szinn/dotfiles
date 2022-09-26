675acb9445214ba4f410248c8a42fbd2583c945b
Scotte Zinn <scotte@zinn.ca>
2022-09-26 03:29:35 -0400

    Added snapshot list

--- home/dot_config/private_fish/omf/hosts/ragnar/functions/zstat.fish
+++ home/dot_config/private_fish/omf/hosts/ragnar/functions/zstat.fish
@@ -1,4 +1,12 @@
 function zstat --description "Statistics on atlas zpool"
-  zpool status -v atlas ; echo " " ; zpool iostat -v ; echo " " ; zfs list -t filesystem ; echo " " ;zfs get compressratio -o all -t filesystem
+  zpool status -v atlas 
+  echo " " 
+  zpool iostat -v 
+  echo " " 
+  zfs list -t filesystem 
+  echo " " 
+  zfs get compressratio -o all -t filesystem
+  echo " "
+  zfs list -t snapshot
 end
 
