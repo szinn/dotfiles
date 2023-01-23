172f6c2f4010cf694add2bbc023fd28cd87e9d8c
Scotte Zinn <scotte@zinn.ca>
2023-01-23 03:12:18 -0500

    Install omf, fisher, and omf plugins

---
+++ home/.chezmoiscripts/run_once_20-install-omf.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+function is_omf_exists() {
+    test -d ~/.local/share/omf &>/dev/null
+}
+
+function install_omf() {
+    if ! is_omf_exists; then
+        curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install > /tmp/install
+        fish /tmp/install --path=~/.local/share/omf --config=~/.config/fish/omf --noninteractive -y
+        rm /tmp/install
+    fi
+}
+
+function main() {
+    install_omf
+}
+
+if [ ${#BASH_SOURCE[@]} = 1 ]; then
+    main
+fi
---
+++ home/.chezmoiscripts/run_once_after_10-install-omf-plugins.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+$(brew --prefix)/bin/fish -c 'omf install'
