d620757cee343b9888a4229cc0b7d436e5fa4978
Scotte Zinn <scotte@zinn.ca>
2023-05-06 15:58:48 -0400

    lvim config

---
+++ home/dot_config/lvim/lua/user/plugins.lua
@@ -0,0 +1,92 @@
+-- Additional Plugins
+lvim.plugins = {
+  "gpanders/editorconfig.nvim",
+  "sainnhe/gruvbox-material",
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "night",
+      disable_background = true,
+      on_highlights = function(hl, _)
+        hl.Normal = {
+          bg = "#000000",
+        }
+        hl.NormalFloat = {
+          bg = "#000000",
+        }
+      end,
+    },
+  },
+  "fatih/vim-go",
+  "olexsmir/gopher.nvim",
+  "j-hui/fidget.nvim",
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup()
+    end,
+  },
+  {
+    "roobert/tailwindcss-colorizer-cmp.nvim",
+    -- optionally, override the default options:
+    config = function()
+      require("tailwindcss-colorizer-cmp").setup {
+        color_square_width = 2,
+      }
+    end,
+  },
+  "lvimuser/lsp-inlayhints.nvim",
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    after = "nvim-treesitter",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+  },
+  "jose-elias-alvarez/typescript.nvim",
+  {
+    "simrat39/symbols-outline.nvim",
+    config = function()
+      require("symbols-outline").setup()
+    end,
+  },
+  "mxsdev/nvim-dap-vscode-js",
+  {
+    "folke/trouble.nvim",
+    dependencies = "nvim-tree/nvim-web-devicons",
+  },
+  "simrat39/rust-tools.nvim",
+  {
+    "saecki/crates.nvim",
+    version = "v0.3.0",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("crates").setup {
+        null_ls = {
+          enabled = true,
+          name = "crates.nvim",
+        },
+        popup = {
+          border = "rounded",
+        },
+      }
+    end,
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    -- cmd = "Copilot",
+    event = "InsertEnter",
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    after = { "copilot.lua" },
+    config = function()
+      require("copilot_cmp").setup()
+    end,
+  },
+  "monaqa/dial.nvim",
+  -- "MunifTanjim/nui.nvim",
+  -- "jackMort/ChatGPT.nvim",
+  -- "Bryley/neoai.nvim"
+}
+
