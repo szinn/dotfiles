ea12846d8f2e25fca46c25087af29d114ab0f54e
Scotte Zinn <scotte@zinn.ca>
2023-05-21 21:20:28 -0400

    tweak df exclusions, lazygit to VeryLazy startup stage

--- home/dot_config/private_fish/omf/hosts/Odin/functions/df.fish
+++ home/dot_config/private_fish/omf/hosts/Odin/functions/df.fish
@@ -1,3 +1,3 @@
 function df --wraps=df --description 'Filtered df'
-  /bin/df -P -kH -T nfs,hfs,apfs,afpfs | grep -v disk1s | ggrep -E -v "/\$" | grep -vi timemachine | ggrep -E -v "Volumes/[VM|Preboot|Update|xarts|iscPreboot]" | grep -vi arqagent
+  /bin/df -P -kH -T nfs,hfs,apfs,afpfs | grep -v disk1s | ggrep -E -v "/\$" | grep -vi timemachine | ggrep -E -v "Volumes/[VM|Hardware|Preboot|Update|xarts|iscPreboot]" | grep -vi arqagent
 end
