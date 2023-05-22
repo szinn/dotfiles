bda0bd31167a7ddb4754870aeeca2d103b654e8e
Scotte Zinn <scotte@zinn.ca>
2023-05-22 08:18:41 -0400

    D'oh - use community packs!

--- home/dot_config/astronvim/lua/user/lsp.lua
+++ home/dot_config/astronvim/lua/user/lsp.lua
@@ -24,18 +24,4 @@ return {
   servers = {
     -- "pyright"
   },
-  setup_handlers = {
-    rust_analyzer = function(_, opts)
-      local extension_path = vim.env.HOME .. "/.vscode/extensions/vadimcn.vscode-lldb-1.9.1/"
-      local codelldb_path = extension_path .. "adapter/codelldb"
-      local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
-
-      require("rust-tools").setup {
-        server = opts,
-        dap = {
-          adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
-        },
-      }
-    end,
-  },
 }
--- home/dot_config/astronvim/lua/user/plugins/community.lua
+++ home/dot_config/astronvim/lua/user/plugins/community.lua
@@ -1,9 +1,10 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  -- example of imporing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.motion.harpoon" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.terraform" },
+  { import = "astrocommunity.pack.yaml" },
 }
