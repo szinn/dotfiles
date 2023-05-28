2cdb9b9f5d728a450a4c57c44272745fb93b67ee
Scotte Zinn <scotte@zinn.ca>
2023-05-28 08:19:58 -0400

    Tweaking tmux/nvim a bit

--- home/dot_config/astronvim/lua/user/plugins/community.lua
+++ home/dot_config/astronvim/lua/user/plugins/community.lua
@@ -2,7 +2,10 @@ return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.motion.harpoon" },
+  { import = "astrocommunity.motion.hop-nvim" },
+  { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.terraform" },
