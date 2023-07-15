a1c4b27dd8ed8964c6ae1fd098093cee3b795e20
Scotte Zinn <scotte@zinn.ca>
2023-07-14 20:53:24 -0400

    Add prune

--- home/dot_config/private_fish/functions/brewup.fish
+++ home/dot_config/private_fish/functions/brewup.fish
@@ -1,11 +1,11 @@
 function brewup --description "Update brew installation"
-  if type -q brew
-    brew update
-    brew upgrade
-    brew cleanup
-    brew doctor
-  end
-  if type -q mas
-    mas upgrade
-  end
+    if type -q brew
+        brew update
+        brew upgrade
+        brew cleanup --prune=all
+        brew doctor
+    end
+    if type -q mas
+        mas upgrade
+    end
 end
