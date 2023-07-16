bdba99d9dd160d7b75da6c13d33bfe9654382d9d
Scotte Zinn <scotte@zinn.ca>
2023-07-16 09:09:47 -0400

    Updates

--- home/dot_config/nvim/lua/plugins/extras/lang/terraform.lua
+++ home/dot_config/nvim/lua/plugins/extras/lang/terraform.lua
@@ -1,26 +1,36 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    event = "BufReadPre",
     opts = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
+      if opts.ensure_installed == nil then
+        opts.ensure_installed = { "hcl", "terraform" }
+      else
         vim.list_extend(opts.ensure_installed, { "hcl", "terraform" })
       end
     end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
+    event = "BufReadPre",
     opts = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
+      if opts.ensure_installed == nil then
+        opts.ensure_installed = { "terraformls" }
+      else
         vim.list_extend(opts.ensure_installed, { "terraformls" })
       end
     end,
   },
   {
     "jay-babu/mason-null-ls.nvim",
+    event = "BufReadPre",
     opts = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
+      if opts.ensure_installed == nil then
+        opts.ensure_installed = { "tflint", "tfsec" }
+      else
         vim.list_extend(opts.ensure_installed, { "tflint", "tfsec" })
       end
+      opts.automatic_installation = true
     end,
   },
 }
