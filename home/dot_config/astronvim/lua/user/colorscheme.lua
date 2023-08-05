fd34a748530a4097c6b29d8c6172f9ad8acd4e25
Scotte Zinn <scotte@zinn.ca>
2023-08-05 14:38:30 -0400

    Remove astronvim config

--- home/dot_config/astronvim/lua/user/colorscheme.lua
+++
@@ -1,6 +0,0 @@
--- Hack for tokyonight
--- vim.api.nvim_create_autocmd({ "User" }, {
---   callback = function(_) vim.cmd [[colorscheme tokyonight]] end,
--- })
--- return "tokyonight"
-return "moonfly"
