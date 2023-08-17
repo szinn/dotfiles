5cb1e9894d472df97a5df5ad5b25da5e40a9393c
Scotte Zinn <scotte@zinn.ca>
2023-08-16 21:00:46 -0400

    Add tmux keys

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -6,5 +6,13 @@ config.font_size = 12
 config.color_scheme = "Dracula (Official)"
 config.window_decorations = "RESIZE"
 config.window_close_confirmation = "NeverPrompt"
+config.hide_tab_bar_if_only_one_tab = true
+
+config.keys = {
+  { key = "g", mods = "CMD", action=wezterm.action{SendString="\x14G"} },
+  { key = "j", mods = "CMD", action=wezterm.action{SendString="\x14T"} },
+  { key = "k", mods = "CMD", action=wezterm.action{SendString="\x14L"} },
+  { key = "t", mods = "CMD", action=wezterm.action{SendString="\x14c"} },
+}
 
 return config
