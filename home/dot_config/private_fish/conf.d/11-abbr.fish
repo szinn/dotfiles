1a869302d97f2decd5a03cd7a1c89194cfbee70a
Scotte Zinn <scotte@zinn.ca>
2023-09-16 17:58:49 -0400

    Updates

--- home/dot_config/private_fish/conf.d/11-abbr.fish
+++ home/dot_config/private_fish/conf.d/11-abbr.fish
@@ -9,12 +9,12 @@ abbr opw 'eval (op signin)'
 abbr tf terraform
 abbr x exit
 
-if type -q exa
-    set -gx EXA_COLORS "da=1;34:gm=1;34"
+if type -q eza
+    set -gx EZA_COLORS "da=1;34:gm=1;34"
 
-    alias l exa
-    alias la 'exa --long --all --group --header --binary --links --inode --blocks'
-    alias ld 'exa --long --all --group --header --list-dirs'
-    alias ll 'exa --long --all --group --header'
-    alias lt 'exa --long --all --group --header --tree --level'
+    alias l eza
+    alias la 'eza --long --all --group --header --binary --links --inode --blocks'
+    alias ld 'eza --long --all --group --header --list-dirs'
+    alias ll 'eza --long --all --group --header'
+    alias lt 'eza --long --all --group --header --tree --level'
 end
