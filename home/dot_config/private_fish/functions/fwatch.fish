455b50a8743fa1ab737e2836ff43e45629241305
Scotte Zinn <scotte@zinn.ca>
2023-09-21 16:50:49 -0400

    Updates

--- home/dot_config/private_fish/functions/fwatch.fish
+++ home/dot_config/private_fish/functions/fwatch.fish
@@ -1,4 +1,4 @@
-function watch --description 'watch with fish alias support'
+function fwatch --description 'watch with fish alias support'
   if count $argv > /dev/null
     command watch -x (which fish) -c "$argv"
   end
