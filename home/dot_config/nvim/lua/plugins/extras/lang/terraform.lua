9d5145806ff04fc687d182dca9bc4913c80046fe
Scotte Zinn <scotte@zinn.ca>
2023-11-12 12:15:10 -0500

    Fix null-ls

--- home/dot_config/nvim/lua/plugins/extras/lang/terraform.lua
+++ home/dot_config/nvim/lua/plugins/extras/lang/terraform.lua
@@ -21,16 +21,16 @@ return {
       end
     end,
   },
-  {
-    "jay-babu/mason-null-ls.nvim",
-    event = "BufReadPre",
-    opts = function(_, opts)
-      if opts.ensure_installed == nil then
-        opts.ensure_installed = { "tflint", "tfsec" }
-      else
-        vim.list_extend(opts.ensure_installed, { "tflint", "tfsec" })
-      end
-      opts.automatic_installation = true
-    end,
-  },
+  --  {
+  --    "jay-babu/mason-null-ls.nvim",
+  --    event = "BufReadPre",
+  --    opts = function(_, opts)
+  --      if opts.ensure_installed == nil then
+  --        opts.ensure_installed = { "tflint", "tfsec" }
+  --      else
+  --        vim.list_extend(opts.ensure_installed, { "tflint", "tfsec" })
+  --      end
+  --      opts.automatic_installation = true
+  --    end,
+  --  },
 }
