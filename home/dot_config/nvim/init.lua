b252dccd79e75aa78c26e8d7b45eaa965cb01acd
Scotte Zinn <scotte@zinn.ca>
2025-01-02 22:08:36 -0500

    Add NeoVIM motions for VSCode

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -1,2 +1,7 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+-- https://medium.com/@nikmas_dev/vscode-neovim-setup-keyboard-centric-powerful-reliable-clean-and-aesthetic-development-582d34297985
+if vim.g.vscode then
+    require "config.vscode_keymaps"
+else
+    -- bootstrap lazy.nvim, LazyVim and your plugins
+    require("config.lazy")
+end
