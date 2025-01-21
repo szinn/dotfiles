913b4ee0733cc0f8a354e661c60f6638d37636ab
Scotte Zinn <scotte@zinn.ca>
2025-01-20 19:37:39 -0500

    Add nvim moonfly theme

--- home/dot_config/nvim/lua/plugins/catppuccin.lua
+++ home/dot_config/nvim/lua/plugins/catppuccin.lua
@@ -1,13 +1,13 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-        colorscheme = "catppuccin-mocha"
-    }
-  }
+  -- {
+  --   "catppuccin/nvim",
+  --   name = "catppuccin",
+  --   priority = 1000,
+  -- },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "catppuccin-mocha"
+  --   },
+  -- },
 }
---
+++ home/dot_config/nvim/lua/plugins/moonfly.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "bluz71/vim-moonfly-colors",
+    name = "moonfly",
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "moonfly",
+    },
+  },
+}
