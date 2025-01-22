b04e52ea1b4de3fad75dd95d489d0f57d7c7a4e8
Scotte Zinn <scotte@zinn.ca>
2025-01-22 18:20:22 -0500

    fix: Use moonfly for floating windows

--- home/dot_config/nvim/lua/config/options.lua
+++ home/dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.g.moonflyNormalFloat = true
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+  border = "single",
+})
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signatureHelp, {
+  border = "single",
+})
+vim.diagnostic.config({ float = { border = "single" } })
--- home/dot_config/nvim/lua/plugins/moonfly.lua
+++ home/dot_config/nvim/lua/plugins/moonfly.lua
@@ -2,6 +2,7 @@ return {
   {
     "bluz71/vim-moonfly-colors",
     name = "moonfly",
+    lazy = false,
     priority = 1000,
   },
   {
