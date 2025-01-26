6696cccc4521ecaa847da3d78abc907fe8520602
Scotte Zinn <scotte@zinn.ca>
2025-01-26 07:45:39 -0500

    hidden visible in neo-tree

--- home/dot_config/nvim/lua/plugins/neo-tree.lua
+++ home/dot_config/nvim/lua/plugins/neo-tree.lua
@@ -3,6 +3,11 @@ return {
     'nvim-neo-tree/neo-tree.nvim',
     opts = {
       close_if_last_window = true,
+      filesystem = {
+        filtered_items = {
+          visible = true,
+        },
+      },
     },
   },
 }
