4ecd8be627fe84030ef4694f22a95115f8c8b476
Scotte Zinn <scotte@zinn.ca>
2025-01-26 08:26:16 -0500

    Minor tweaks

--- home/dot_config/private_fish/conf.d/01-abbr.fish
+++ home/dot_config/private_fish/conf.d/01-abbr.fish
@@ -1,5 +1,8 @@
 abbr --add -- ap ansible-playbook
 abbr --add -- apb 'ansible-playbook --ask-become'
+abbr --add -- cma 'chezmoi apply'
+abbr --add -- cms 'chezmoi status'
+abbr --add -- cmu 'chezmoi unmanaged'
 abbr --add -- gfp 'git fetch -p && git pull'
 abbr --add -- gitp 'git push'
 abbr --add -- gitpf 'git push -f'
