34efb881ba48577a466fbaf5184eadc673d2c7f2
Scotte Zinn <scotte@zinn.ca>
2025-03-02 12:53:12 -0500

    fix(fish): Refactor conf.d

--- home/dot_config/private_fish/conf.d/01-abbr.fish
+++ home/dot_config/private_fish/conf.d/01-abbr.fish
@@ -1,10 +1,2 @@
-abbr --add -- ap ansible-playbook
-abbr --add -- apb 'ansible-playbook --ask-become'
-abbr --add -- cma 'chezmoi apply'
-abbr --add -- cms 'chezmoi status'
-abbr --add -- cmu 'chezmoi unmanaged'
-abbr --add -- gfp 'git fetch -p && git pull'
-abbr --add -- gitp 'git push'
-abbr --add -- gitpf 'git push -f'
 abbr --add -- t task
 abbr --add -- tf terraform
