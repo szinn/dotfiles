dcb9a93977bdee6d0f278715ee31776438d94e09
Scotte Zinn <scotte@zinn.ca>
2025-03-19 14:08:42 -0400

    VSCode config

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -1,5 +1,6 @@
 -- https://medium.com/@nikmas_dev/vscode-neovim-setup-keyboard-centric-powerful-reliable-clean-and-aesthetic-development-582d34297985
 if vim.g.vscode then
+    -- running neovim in VSCode
     require("config.vscode_keymaps")
 else
     -- bootstrap lazy.nvim, LazyVim and your plugins
